3d47b5564ec87cce4084214d8cc65a142bdf165d
Ziyodilla <ziyodillajz@gmail.com>
2022-09-18 23:36:10 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        header: "#040813;",
+        linkHover: "#ffffffb3",
+        gradient: "#5ADFE5",
+      }
+    },
+  },
+  plugins: [],
+}
